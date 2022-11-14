2e55ae123aadb7072465ef918166c80ab4f4a24e
Ethan <etgeorge@davidson.edu>
2022-11-14 14:30:06 -0500

    Last changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 
 /* javascript */
 
-const getAuthorization = (function () {
-    const clientId = '619dc4ae72bb482c9627b588fc4cba36';
-    const clientSecret = '551dfacf8408410caf30a92880e99a1f';
+const clientId = '619dc4ae72bb482c9627b588fc4cba36';
+const clientSecret = '551dfacf8408410caf30a92880e99a1f';
 
+const getAuthorization = (function () {
     var url = 'https://accounts.spotify.com/authorize?'
     url += 'client_id=' + clientId;
     url += '&response_type=code';
@@ -28,5 +28,12 @@ function getCode()  {
     return code;
 }
 
+function handleRedirect (){
+    let code = getCode();
+    getToken(code);
+}
 
-console.log('Here is the code: ' +getCode());
+
+function getToken(code){
+    let body = code;
+}
