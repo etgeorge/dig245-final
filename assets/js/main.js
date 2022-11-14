b60a767e88e34f82aacc51df46a3c5017d5b1ec2
Ethan <etgeorge@davidson.edu>
2022-11-14 11:22:13 -0500

    Setting up web api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,31 @@
 
 /* javascript */
+const getAuthorization = (function () {
+
+    const clientId = '619dc4ae72bb482c9627b588fc4cba36';
+    const clientSecret = '551dfacf8408410caf30a92880e99a1f';
+
+    var url = 'https://accounts.spotify.com/authorize?'
+    url += 'client_id=' + clientId;
+    url += '&response_type=code';
+    url += '&redirect_uri=https://etgeorge.github.io/dig245-final/';
+
+    window.location.href=url;
+
+    
+})();
+
+function getCode()  {
+    let code = null;
+    const searchQuery = window.location.search;
+
+    if (searchQuery.length > 0){
+        const urlParams = new URLSearchParams(searchQuery);
+        code = urlParams.get('code');
+    }
+
+    return code;
+}
+
+
+console.log('Here is the code: ' +getCode());
